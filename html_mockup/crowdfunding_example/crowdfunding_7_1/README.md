28fac9b0c4884d25b6693aaeb0b77db5587a5d29
Armando Escalier <pepeescalier@hotmail.com>
2023-08-28 15:15:43 -0600

    7-1 methodology example

---
+++ html_mockup/crowdfunding_example/crowdfunding_7_1/README.md
@@ -0,0 +1,27 @@
+# crowdfund
+
+Crowfund es un ejercicio de maquetación web, utilizando la metodología 7-1 en CSS.
+
+### Sistema de archivos 7-1.
+El "7" se refiere a siete directorios temáticos, el "1": un único archivo .scss compilado en la hoja de estilos CSS.
+
+- Base
+- Utils
+- Layout
+- Components
+- Pages
+- Themes
+- Vendors
+
+## Instalación
+
+Descarga la carpeta "crowfunding_7_1"
+
+## Uso
+
+El sitio web debería mostrarse al abrir "index.html" en un navegador web.
+
+## IMPORTANTE
+-Es necesario tener conexión a internet para que el tipo de font funcione correctamente.
+
+-El proyecto utiliza SASS para CSS, es posible que sea necesario instalarlo para el correcto funcionamiento del sitio web.
---
+++ html_mockup/crowdfunding_example/crowdfunding_7_1/assets/scripts/script.js
@@ -0,0 +1,140 @@
+
+// Menu 
+const hamburger = document.querySelector(".hamburger-menu");
+const navMenu = document.querySelector(".navbar__menu");
+const body = document.body;
+
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+    body.classList.toggle("block-scroll");
+}
+
+const navLink = document.querySelectorAll(".navbar__link");
+
+navLink.forEach(n => n.addEventListener("click", closeMenu));
+
+function closeMenu() {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+    body.classList.remove("block-scroll");
+}
+
+
+// close modal X change color
+function closeModalChangeIconColor() {
+    document.getElementsByClassName("close-modal").style.fill = '#000';
+}
+
+
+// Bookmark Color
+let first_click = true;
+
+function bookmarkClick() {
+    if (first_click) {
+        document.getElementById("bookmark-circle").style.fill = 'hsl(176, 72%, 28%)';
+        document.getElementById("bookmark-path").style.fill = '#fff';
+        document.getElementById("bookmark-button").textContent = 'Bookmarked';
+        document.getElementById("bookmark-button").style.color = 'hsl(176, 72%, 28%)';
+        first_click = false;
+    } else {
+        document.getElementById("bookmark-circle").style.fill = '#2F2F2F';
+        document.getElementById("bookmark-path").style.fill = '#fff';
+        document.getElementById("bookmark-button").textContent = 'Bookmark';
+        document.getElementById("bookmark-button").style.color = 'hsl(0, 0%, 48%)';
+        first_click = true;
+    }
+}
+
+// Bookmark icon change color on hover
+function bookmarkIconColorEnter() {
+    if (first_click) {
+        document.getElementById("bookmark-circle").style.fill = 'hsl(0, 0%, 48%)';
+        document.getElementById("bookmark-path").style.fill = 'hsl(0, 0%, 70%)';
+    }
+}
+
+function bookmarkIconColorLeave() {
+    if (first_click) {
+        document.getElementById("bookmark-circle").style.fill = '#2F2F2F';
+        document.getElementById("bookmark-path").style.fill = '#B1B1B1';
+    }
+}
+
+// Radio Button and package color
+
+function radioButtonsChecked() {
+    if (document.getElementById('radio-pack1').checked) {
+        document.getElementById("pack1").style.borderWidth = '2px';
+        document.getElementById("pack1").style.borderColor = 'hsl(176, 50%, 47%)';
+        document.getElementById("pack2").style.borderWidth = '1px';
+        document.getElementById("pack2").style.borderColor = 'hsl(0, 0%, 70%)';
+        document.getElementById("pack3").style.borderWidth = '1px';
+        document.getElementById("pack3").style.borderColor = 'hsl(0, 0%, 70%)';
+        document.getElementById("enter-pledge1").style.display = 'flex';
+        document.getElementById("enter-pledge2").style.display = 'none';
+        document.getElementById("enter-pledge3").style.display = 'none';
+        document.getElementById("input-pack1").focus();
+    } else if (document.getElementById('radio-pack2').checked) {
+        document.getElementById("pack1").style.borderWidth = '1px';
+        document.getElementById("pack1").style.borderColor = 'hsl(0, 0%, 70%)';
+        document.getElementById("pack3").style.borderWidth = '1px';
+        document.getElementById("pack3").style.borderColor = 'hsl(0, 0%, 70%)';
+        document.getElementById("pack2").style.borderWidth = '2px';
+        document.getElementById("pack2").style.borderColor = 'hsl(176, 50%, 47%)';
+        document.getElementById("enter-pledge2").style.display = 'flex';
+        document.getElementById("enter-pledge1").style.display = 'none';
+        document.getElementById("enter-pledge3").style.display = 'none';
+        document.getElementById("input-pack2").focus();
+    } else if (document.getElementById('radio-pack3').checked) {
+        document.getElementById("pack1").style.borderWidth = '1px';
+        document.getElementById("pack1").style.borderColor = 'hsl(0, 0%, 70%)';
+        document.getElementById("pack2").style.borderWidth = '1px';
+        document.getElementById("pack2").style.borderColor = 'hsl(0, 0%, 70%)';
+        document.getElementById("pack3").style.borderWidth = '2px';
+        document.getElementById("pack3").style.borderColor = 'hsl(176, 50%, 47%)';
+        document.getElementById("enter-pledge3").style.display = 'flex';
+        document.getElementById("enter-pledge1").style.display = 'none';
+        document.getElementById("enter-pledge2").style.display = 'none';
+        document.getElementById("input-pack3").focus();
+    }
+}
+
+// checked radio buttons with click on pack titles 
+function titlePacksOnClick(packNumber) {
+    let sanitizedNumber = packNumber.replace(/[^0-9\.]+/g, '');
+
+    document.getElementById("radio-pack" + sanitizedNumber).checked = true;
+    radioButtonsChecked();
+}
+
+// Close modal by click outside
+const btn = document.getElementsByClassName("basic-button");
+const modal = document.getElementById("main-modal");
+const closeModal = document.getElementsByClassName("close-modal");
+
+Array.prototype.forEach.call(btn, function (element) {
+    element.addEventListener('click', function () {
+        modal.style.display = "block";
+        // Add block scroll to body 
+        if(!body.classList.contains("block-scroll")){
+            body.classList.toggle("block-scroll");
+        }
+    });
+});
+
+Array.prototype.forEach.call(closeModal, function (element) {
+    element.addEventListener('click', function () {
+        modal.style.display = "none";
+        body.classList.remove("block-scroll");
+    });
+});
+
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+        body.classList.remove("block-scroll");
+    }
+}
